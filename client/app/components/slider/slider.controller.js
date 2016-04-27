e1f5510a1c5dbf2e8f73b7a066daf8d0ee64fd3d
AlexPlex <atellmer@gmail.com>
2016-04-27 21:31:50 +0500

    slider carousel added

---
+++ client/app/components/slider/slider.controller.js
@@ -0,0 +1,110 @@
+;
+(function () {
+	'use strict';
+
+	angular
+		.module('app')
+		.controller('SliderController', SliderController);
+
+	SliderController.$inject = ['$interval'];
+
+	function SliderController($interval) {
+		var vm = this;
+		
+		var slider = angular.element(document.querySelector('#vit-slider-content'));
+		
+		vm.changeContent = changeContent;
+		
+		
+		activate();
+		
+		
+		function activate() {
+			$interval(function() {
+				changeContent(1);
+			}, 8000);
+			
+			hideAllUnlessFirst();
+		}
+		
+		function changeContent(count) {
+			
+			var children = slider.children();
+			
+			for(var i = 0; i < children.length; i++) {
+				if(!angular.element(children[i]).hasClass('display-hide')) {
+					angular.element(children[i]).addClass('display-hide')
+					
+					if(count > 0) {
+						if(children[i+1]) {
+							if(angular.element(children[i+1]).hasClass('slide-left')) {
+								angular.element(children[i+1])
+									.removeClass('display-hide')
+									.removeClass('slide-left')
+									.addClass('slide-right');
+							} else {
+								angular.element(children[i+1])
+									.removeClass('display-hide')
+									.addClass('slide-right');
+							}
+								
+							break;
+						} else {
+							if(angular.element(children[0]).hasClass('slide-left')) {
+								angular.element(children[0])
+									.removeClass('display-hide')
+									.removeClass('slide-left')
+									.addClass('slide-right');
+							} else {
+								angular.element(children[0])
+									.removeClass('display-hide')
+									.addClass('slide-right');
+							}
+							break;
+						}
+						
+					} else {
+						if(children[i-1]) {
+							
+							if(angular.element(children[i-1]).hasClass('slide-right')) {
+								angular.element(children[i-1])
+									.removeClass('display-hide')
+									.removeClass('slide-right')
+									.addClass('slide-left');
+							} else {
+								angular.element(children[i-1])
+									.removeClass('display-hide')
+									.addClass('slide-left');
+							}
+							
+							break;
+						} else {
+							if(angular.element(children[children.length - 1]).hasClass('slide-right')) {
+								angular.element(children[children.length - 1])
+									.removeClass('display-hide')
+									.removeClass('slide-right')
+									.addClass('slide-left');
+							} else {
+								angular.element(children[children.length - 1])
+									.removeClass('display-hide')
+									.addClass('slide-left');
+							}
+							break;
+						}
+					}
+				}
+			}
+		}	
+		
+		function hideAllUnlessFirst() {
+			var children = slider.children();
+			
+			for(var i = 1; i < children.length; i++) {
+				if(!angular.element(children[i]).hasClass('display-hide')) {
+					angular.element(children[i]).addClass('display-hide')
+				}
+			}
+			
+		}
+	}
+})();
