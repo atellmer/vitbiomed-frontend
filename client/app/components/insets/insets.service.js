61d4c05f8ab71d4fa4e55a0bca101b8513a1a8db
AlexPlex <atellmer@gmail.com>
2016-05-25 20:01:11 +0500

    product-card finished

---
+++ client/app/components/insets/insets.service.js
@@ -0,0 +1,40 @@
+;
+(function() {
+'use strict';
+
+	angular
+		.module('app')
+		.factory('lkInsets', lkInsets);
+
+	lkInsets.$inject = ['lkFunctions'];
+	
+	function lkInsets(lkFunctions) {
+		var service = {
+			init: init,
+			control: control
+		};
+		
+		var click = [];
+		
+		return service;
+
+		////////////////
+		function init() { 
+			click = lkFunctions.makeArrayOf(0, document.querySelectorAll('[data-inset-trigger]').length);
+		}
+		
+		function control(event) {		
+			var trigger = angular.element(event.target);
+			var id = parseInt(trigger.attr('data-inset-trigger'));
+			var target = angular.element(document.querySelector('[data-inset-target="' + id + '"]'));
+			
+			click[id]++;
+			
+			if (click[id] % 2) {
+				target.show(200);
+			} else {
+				target.hide(200);
+			}
+		}
+	}
+})();
--- client/app/components/product-item-page/product-item-page.controller.js
+++ client/app/components/product-item-page/product-item-page.controller.js
@@ -6,18 +6,17 @@
 		.module('app')
 		.controller('ProductItemPageController', ProductItemPageController);
 
-	ProductItemPageController.$inject = ['ngDialog', 'lkFunctions', 'lkSelectProduct'];
+	ProductItemPageController.$inject = ['ngDialog', 'lkFunctions', 'lkSelectProduct', 'lkInsets'];
 
-	function ProductItemPageController(ngDialog, lkFunctions, lkSelectProduct) {
+	function ProductItemPageController(ngDialog, lkFunctions, lkSelectProduct, lkInsets) {
 		var vm = this;
-		var click = [];
 		
 		vm.product = lkSelectProduct.product;
 		vm.setAmount = lkSelectProduct.setAmount;
 		vm.setVolume = lkSelectProduct.setVolume;
 		vm.getAmount = lkSelectProduct.getAmount;
 		
-		vm.controlInset = controlInset;
+		vm.controlInset = lkInsets.control;
 		vm.clickToOpen = clickToOpen;
 		
 		activate();
@@ -25,21 +24,7 @@
 		function activate() {
 			lkSelectProduct.init();
 			
-			click = lkFunctions.makeArrayOf(0, document.querySelectorAll('[data-description-trigger]').length);
-		}
-		
-		function controlInset(event) {				
-			var trigger = angular.element(event.target);
-			var id = parseInt(trigger.attr('data-description-trigger'));
-			var target = angular.element(document.querySelector('[data-description-target="' + id + '"]'));
-			
-			click[id]++;
-			
-			if (click[id] % 2) {
-				target.show(200);
-			} else {
-				target.hide(200);
-			}
+			lkInsets.init();
 		}
 
 		function clickToOpen(templateId) {
