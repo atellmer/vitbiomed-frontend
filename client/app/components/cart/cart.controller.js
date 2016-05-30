fe62a8028680711d97aa89f9bd475b27ff999729
AlexPlex <atellmer@gmail.com>
2016-05-30 19:55:56 +0500

    small fix

--- client/app/components/cart/cart.controller.js
+++ client/app/components/cart/cart.controller.js
@@ -10,17 +10,23 @@
 	
 	function CartController(lkCart) {
 		var vm = this;
-
+		
+		vm.open = open;
 		vm.amount = lkCart.getAmount;
 		vm.getGeneralSum = lkCart.getGeneralSum;
 		vm.products = lkCart.store;
 		vm.removeItem = lkCart.removeItem;
 		
-
+		var cart = angular.element(document.querySelector('.js-cart-action'));
+		
 		activate();
 
 		////////////////
 
 		function activate() {}
+		
+		function open() {
+			cart.toggleClass('js-cart-action--active');
+		}
 	}
 })();
