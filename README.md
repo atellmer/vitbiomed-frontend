64f9cb72bd4ee0c215775606c04261101c9ebd9b
atellmer <atellmer@gmail.com>
2017-01-25 11:38:06 +0500

    gh pages

--- README.md
+++
@@ -1,1 +0,0 @@
-# vitbiomed-frontend
--- config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-module.exports = {
-	mode: process.env.NODE_ENV !== 'production' ? 'development' : 'production',
-	debug: process.env.NODE_ENV !== 'production' ? true : false
-}
